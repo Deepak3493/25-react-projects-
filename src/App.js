98eccf3ae1bc6f57329d8eb1f36270b6511cac93
deepakojha <deepak.ojha@protium.co.in>
2023-05-20 02:33:11 +0530

    timer app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useRef } from "react";
+/* this  is to learn 
+useState, 
+useRef ans 
+setInterval and 
+clearInterval */
 function App() {
+  const [timeLeft, setTimeLeft] = useState(10 * 60);
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+  const intervalRef = useRef(null);
+  function padTime(time) {
+    return time.toString().padStart(2, "0");
+  }
+  function startTimer() {
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        return timeLeft <= 0 ? resetTimer() : timeLeft - 1;
+      });
+    }, 1000);
+  }
+  function stopTimer() {
+    clearInterval(intervalRef.current);
+  }
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    setTimeLeft(10 * 60);
+    return 0;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="">react timer project</div>
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="" style={{ magin: "5px", padding: "5px" }}>
+        <button onClick={startTimer}>start</button>
+        <button onClick={stopTimer}>stop</button>
+        <button onClick={resetTimer}>reset</button>
+      </div>
     </div>
   );
 }
